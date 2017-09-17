841327f8f159ef302ea74d1c69291c288b3febb6
kattee <katteegorska@poczta.onet.pl>
2017-09-17 12:34:00 +0200

    first commit

---
+++ src/main/java/pl/projekt/kasia/ProjektStart.java
@@ -0,0 +1,8 @@
+package pl.projekt.kasia;
+
+public class ProjektStart {
+    public static void main(String[] args){
+        System.out.println("Testy");
+    }
+}
+
---
+++ src/test/java/pl/projekt/kasia/ProjektStart1.java
@@ -0,0 +1,39 @@
+package pl.projekt.kasia;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class ProjektStart1 {
+    static FirefoxDriver driver;
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void mainTest(){
+        driver.get("http://newtours.demoaut.com");
+
+        driver.findElement(By.name("userName")).clear();
+        driver.findElement(By.name("userName")).sendKeys("Kasia");
+        driver.findElement(By.name("password")).clear();
+        driver.findElement(By.name("password")).sendKeys("kasia123");
+        driver.findElement(By.name("login")).click();
+
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        driver.close();
+        driver.quit();
+    }
+}
