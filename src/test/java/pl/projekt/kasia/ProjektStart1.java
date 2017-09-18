993e6ec8ae203e0f8c11614a3fe9782264543a31
kattee <katteegorska@poczta.onet.pl>
2017-09-18 19:09:08 +0200

    first commit

--- src/test/java/pl/projekt/kasia/ProjektStart1.java
+++ src/test/java/pl/projekt/kasia/ProjektStart1.java
@@ -5,6 +5,7 @@ import org.junit.BeforeClass;
 import org.junit.Ignore;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import java.util.concurrent.TimeUnit;
@@ -14,6 +15,7 @@ public class ProjektStart1 {
 
     @BeforeClass
     public static void openBrowser(){
+        System.setProperty("webdriver.gecko.driver", "C:\\WORK\\geckodriver.exe");
         driver = new FirefoxDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.manage().window().maximize();
